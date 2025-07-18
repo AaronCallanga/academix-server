b90b6cc7f4899e4ff8af0f1173a7f4b7a0074dc8
pogi <aaroncallanga01@gmail.com>
2025-07-19 07:43:23 +0800

    DocumentRemark entity

---
+++ src/main/java/com/academix/academix/document/entity/DocumentRemark.java
@@ -0,0 +1,40 @@
+package com.academix.academix.document.entity;
+
+import com.academix.academix.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentRemark {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private String role;        // ADMIN, REGISTRAR, STUDENT
+    private LocalDateTime timeStamp;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
+    private DocumentRequest documentRequest;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;        // User who gives the remarks
+}
