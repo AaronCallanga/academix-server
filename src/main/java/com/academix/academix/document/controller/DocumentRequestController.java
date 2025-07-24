1cf8b487cc357d8328759c4f5600053cb21684df
pogi <aaroncallanga01@gmail.com>
2025-07-24 12:57:38 +0800

    DocumentRequestController - basic set up

--- src/main/java/com/academix/academix/document/controller/DocumentRequestController.java
+++ src/main/java/com/academix/academix/document/controller/DocumentRequestController.java
@@ -1,11 +1,17 @@
 package com.academix.academix.document.controller;
 
+import com.academix.academix.document.service.api.DocumentRequestService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/documents")
+@RequiredArgsConstructor
 public class DocumentRequestController {
+
+    private final DocumentRequestService documentRequestService;
+
 }
 
 /*
