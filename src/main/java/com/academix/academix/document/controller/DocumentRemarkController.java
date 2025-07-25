5fc38636672e58833b664dfc3e322709a9476762
pogi <aaroncallanga01@gmail.com>
2025-07-25 11:31:18 +0800

    DocumentRemarkController - updateDocumentRemark

--- src/main/java/com/academix/academix/document/controller/DocumentRemarkController.java
+++ src/main/java/com/academix/academix/document/controller/DocumentRemarkController.java
@@ -1,12 +1,15 @@
 package com.academix.academix.document.controller;
 
+import com.academix.academix.document.dto.request.DocumentRemarkRequestDTO;
 import com.academix.academix.document.dto.response.DocumentRemarkResponseDTO;
 import com.academix.academix.document.service.api.DocumentRemarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +24,13 @@ public class DocumentRemarkController {
 
     @GetMapping("/{requestId}")
     public ResponseEntity<List<DocumentRemarkResponseDTO>> getAllDocumentRemarksByRequestId(@PathVariable Long requestId) {
-        // Fetch the the list by ID
         List<DocumentRemarkResponseDTO> documentRemarks = documentRemarkService.getAllDocumentRemarksByRequestId(requestId);
         return new ResponseEntity<>(documentRemarks, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{remarksId}")
+    public ResponseEntity<DocumentRemarkResponseDTO> updateDocumentRemark(@PathVariable Long remarksId, @RequestBody DocumentRemarkRequestDTO documentRemarkRequestDTO) {
+        DocumentRemarkResponseDTO documentRemarkResponseDTO = documentRemarkService.updateRemark(documentRemarkRequestDTO, remarksId);
+        return new ResponseEntity<>(documentRemarkResponseDTO, HttpStatus.OK);
     }
 }
