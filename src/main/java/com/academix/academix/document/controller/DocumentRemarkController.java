5a272dc8f8f75ab5edadda858cc890a9ee809660
pogi <aaroncallanga01@gmail.com>
2025-07-25 11:36:55 +0800

    DocumentRemarkController - addDocumentRemark

--- src/main/java/com/academix/academix/document/controller/DocumentRemarkController.java
+++ src/main/java/com/academix/academix/document/controller/DocumentRemarkController.java
@@ -6,9 +6,11 @@ import com.academix.academix.document.service.api.DocumentRemarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +31,14 @@ public class DocumentRemarkController {
     }
 
     @PatchMapping("/{remarksId}")
-    public ResponseEntity<DocumentRemarkResponseDTO> updateDocumentRemark(@PathVariable Long remarksId, @RequestBody DocumentRemarkRequestDTO documentRemarkRequestDTO) {
+    public ResponseEntity<DocumentRemarkResponseDTO> updateDocumentRemark(@RequestBody DocumentRemarkRequestDTO documentRemarkRequestDTO, @PathVariable Long remarksId) {
         DocumentRemarkResponseDTO documentRemarkResponseDTO = documentRemarkService.updateRemark(documentRemarkRequestDTO, remarksId);
+        return new ResponseEntity<>(documentRemarkResponseDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/documents/{requestId}")
+    public ResponseEntity<DocumentRemarkResponseDTO> addDocumentRemark(@RequestBody DocumentRemarkRequestDTO documentRemarkRequestDTO, @PathVariable Long requestId, Authentication authentication) {
+        DocumentRemarkResponseDTO documentRemarkResponseDTO = documentRemarkService.addRemark(requestId, documentRemarkRequestDTO, authentication);
         return new ResponseEntity<>(documentRemarkResponseDTO, HttpStatus.OK);
     }
 }
