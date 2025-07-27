1239c4148b4343fa14bbcbdae470a3271f2862d0
pogi <aaroncallanga01@gmail.com>
2025-07-27 11:49:56 +0800

    DocumentRequestRepository - Implementing Paging

--- src/main/java/com/academix/academix/document/repository/DocumentRequestRepository.java
+++ src/main/java/com/academix/academix/document/repository/DocumentRequestRepository.java
@@ -1,6 +1,8 @@
 package com.academix.academix.document.repository;
 
 import com.academix.academix.document.entity.DocumentRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface DocumentRequestRepository extends JpaRepository<DocumentRequest, Long> {
-    List<DocumentRequest> findByRequestedById(Long userId);
-    List<DocumentRequest> findByRequestedByEmail(String email);
+    Page<DocumentRequest> findByRequestedById(Long userId, Pageable pageable);
+    Page<DocumentRequest> findByRequestedByEmail(String email, Pageable pageable);
 }
