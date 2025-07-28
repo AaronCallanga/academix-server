9988f2307f0cf9f12091d72c171ba57ea717d4a8
pogi <aaroncallanga01@gmail.com>
2025-07-28 12:51:41 +0800

    DocumentRemarkRepository - pagination implemented

--- src/main/java/com/academix/academix/document/repository/DocumentRemarkRepository.java
+++ src/main/java/com/academix/academix/document/repository/DocumentRemarkRepository.java
@@ -1,6 +1,8 @@
 package com.academix.academix.document.repository;
 
 import com.academix.academix.document.entity.DocumentRemark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface DocumentRemarkRepository extends CrudRepository<DocumentRemark, Long> {
-    List<DocumentRemark> findByDocumentRequestIdOrderByTimeStampAsc(Long documentRequestId);
+    Page<DocumentRemark> findByDocumentRequestIdOrderByTimeStampAsc(Long documentRequestId, Pageable pageable);
 }
