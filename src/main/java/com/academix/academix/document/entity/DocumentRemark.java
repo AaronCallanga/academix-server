48393d9fd2f21bacde3b6c04d75018a5fb6449be
pogi <aaroncallanga01@gmail.com>
2025-07-29 19:01:34 +0800

    Reverted to String Document Remark Role

--- src/main/java/com/academix/academix/document/entity/DocumentRemark.java
+++ src/main/java/com/academix/academix/document/entity/DocumentRemark.java
@@ -1,11 +1,8 @@
 package com.academix.academix.document.entity;
 
-import com.academix.academix.document.enums.DocumentRemarkRole;
 import com.academix.academix.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,8 +14,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
@@ -38,8 +33,7 @@ public class DocumentRemark {
     private String content;
 
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private DocumentRemarkRole role;        // ADMIN, REGISTRAR, STUDENT
+    private String role;        // ADMIN, REGISTRAR, STUDENT
 
     @Column(nullable = false)
     private LocalDateTime timeStamp;
