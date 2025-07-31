7ab9a4bd3c39280543f9f158ee2879bcb3053951
pogi <aaroncallanga01@gmail.com>
2025-07-31 18:51:13 +0800

    Added ReasonDTO for clarity

---
+++ src/main/java/com/academix/academix/document/dto/request/ReasonDTO.java
@@ -0,0 +1,12 @@
+package com.academix.academix.document.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ReasonDTO {
+    String reason;
+}
