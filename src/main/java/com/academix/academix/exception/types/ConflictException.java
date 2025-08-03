e5bd91dd268f02a3c6a8c31f1d35019f567d57fa
pogi <aaroncallanga01@gmail.com>
2025-08-03 11:19:40 +0800

    ConflictException Class

---
+++ src/main/java/com/academix/academix/exception/types/ConflictException.java
@@ -0,0 +1,7 @@
+package com.academix.academix.exception.types;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
