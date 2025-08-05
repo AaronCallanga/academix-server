8d5bf476ee12a80d5e713702d1e6377930b010e6
pogi <aaroncallanga01@gmail.com>
2025-08-05 18:13:12 +0800

    AdminDocumentRequestFacade interface

---
+++ src/main/java/com/academix/academix/document/facade/api/AdminDocumentRequestFacade.java
@@ -0,0 +1,21 @@
+package com.academix.academix.document.facade.api;
+
+import com.academix.academix.document.dto.request.DocumentRequestAdminUpdateDTO;
+import com.academix.academix.document.dto.request.ReasonDTO;
+import com.academix.academix.document.dto.response.DocumentRequestResponseDTO;
+import com.academix.academix.document.dto.response.DocumentRequestResponseListDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
+
+public interface AdminDocumentRequestFacade {
+    // ==== ADMIN / REGISTRAR ====
+    Page<DocumentRequestResponseListDTO> getAllDocumentRequests(int page, int size, String sortField, String sortDirection);
+    Page<DocumentRequestResponseListDTO> getUserDocumentRequests(Long userId, int page, int size, String sortField, String sortDirection);
+    DocumentRequestResponseDTO approveDocumentRequest(Long documentRequestId, Authentication authentication);
+    DocumentRequestResponseDTO rejectDocumentRequest(Long documentRequestId, Authentication authentication, ReasonDTO reasonDto);
+    DocumentRequestResponseDTO releaseDocumentRequest(Long documentRequestId, Authentication authentication);
+    DocumentRequestResponseDTO setDocumentRequestStatusToReadyForPickup(Long documentRequestId, Authentication authentication);
+    DocumentRequestResponseDTO setDocumentRequestStatusToInProgress(Long documentRequestId, Authentication authentication);
+    /** @NOTE: Use for FORCE update for status and pick up date  - UI shows current status choice, use this to change the pick-up date */
+    DocumentRequestResponseDTO adminUpdateDocumentRequest(Long documentRequestId, DocumentRequestAdminUpdateDTO documentRequestAdminUpdateDTO, Authentication authentication);
+}
