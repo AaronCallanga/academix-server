52b2cbb7b22c6138b004f203ee4589cd2c901892
pogi <aaroncallanga01@gmail.com>
2025-08-07 10:23:26 +0800

    DocumentRequestAuditController - getAllAuditLogsByRequestId

--- src/main/java/com/academix/academix/log/controller/DocumentRequestAuditController.java
+++ src/main/java/com/academix/academix/log/controller/DocumentRequestAuditController.java
@@ -5,6 +5,7 @@ import com.academix.academix.log.entity.DocumentRequestAudit;
 import com.academix.academix.log.repository.DocumentRequestAuditRepository;
 import com.academix.academix.log.service.api.DocumentRequestAuditService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +24,8 @@ public class DocumentRequestAuditController {
     public Page<DocumentRequestAuditResponseDTO> getAllAuditLogsByRequestId(@PathVariable Long requestId,
                                                                             @RequestParam(defaultValue = "0") int page,
                                                                             @RequestParam(defaultValue = "10") int size,
-                                                                            @RequestParam(defaultValue = "ASC") String sortDirection,
-                                                                            @RequestParam(defaultValue = "requestDate") String sortField) {
+                                                                            @RequestParam(defaultValue = "DESC") String sortDirection,
+                                                                            @RequestParam(defaultValue = "performedAt") String sortField) {
         return documentRequestAuditService.getAllDocumentRequestsByRequestId(requestId, page, size, sortDirection, sortField);
     }
 
