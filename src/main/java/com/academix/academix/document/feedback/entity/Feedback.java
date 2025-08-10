02128d3e5010197dbad434d586301d65f87ddd0d
pogi <aaroncallanga01@gmail.com>
2025-08-10 19:42:24 +0800

    Feedback entity

--- src/main/java/com/academix/academix/document/feedback/entity/Feedback.java
+++ src/main/java/com/academix/academix/document/feedback/entity/Feedback.java
@@ -1,20 +1,43 @@
 package com.academix.academix.document.feedback.entity;
 
+import com.academix.academix.document.request.entity.DocumentRequest;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Feedback {
     @Id
     @GeneratedValue
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "document_request_id", nullable = false)
+    private DocumentRequest documentRequest;
+
+    private int rating;
+
+    private String comment;
+
+    private boolean anonymous;
+
+    private LocalDateTime submittedAt;
+
+
 }
