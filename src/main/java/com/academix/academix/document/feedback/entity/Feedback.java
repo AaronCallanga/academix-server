feb53472996db864f8c3c3000a32827a2618ea37
pogi <aaroncallanga01@gmail.com>
2025-08-10 19:03:16 +0800

    set up feedback

--- src/main/java/com/academix/academix/document/feedback/entity/Feedback.java
+++ src/main/java/com/academix/academix/document/feedback/entity/Feedback.java
@@ -1,4 +1,4 @@
-package com.academix.academix.feedback.entity;
+package com.academix.academix.document.feedback.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
