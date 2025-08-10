bdaa756ff024cc5f22504183b326965f8071b811
pogi <aaroncallanga01@gmail.com>
2025-08-10 20:33:07 +0800

    FeedbackService set up

--- src/main/java/com/academix/academix/document/feedback/service/api/FeedbackService.java
+++ src/main/java/com/academix/academix/document/feedback/service/api/FeedbackService.java
@@ -1,7 +1,10 @@
 package com.academix.academix.document.feedback.service.api;
 
 import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
+import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
 import com.academix.academix.document.feedback.entity.Feedback;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
 
 import java.util.List;
@@ -9,10 +12,18 @@ import java.util.List;
 public interface FeedbackService {
     // Common
     Feedback submitFeedback(FeedbackRequestDTO feedbackRequestDTO, Authentication authentication);
-    Feedback submitFeedbackAnonymously(FeedbackRequestDTO feedbackRequestDTO);
     Feedback getFeedbackByRequestId(Long requestId);
+    List<FeedbackResponseDTO> getOwnFeedbacks(Authentication authentication);
 
     // Admin
-    List<Feedback> getAllFeedbacks();
+    Page<Feedback> getAllFeedbacks(PageRequest pageRequest);
+    Page<Feedback> getFeedbacksByRating(int rating, PageRequest pageRequest);
 
+
+    /* make this private?
+    // Utility / Management
+    double getAverageRating();
+    Map<Integer, Long> getRatingDistribution(); // e.g., {1: 3, 2: 5, 5: 10}    count nubmer of response per rating level
+    boolean hasUserSubmittedFeedback(Long requestId, Authentication authentication);
+     */
 }
