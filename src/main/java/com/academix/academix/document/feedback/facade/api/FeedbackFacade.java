affc97a286ce9466dcd22ce1f8607f02f2b0ea7a
pogi <aaroncallanga01@gmail.com>
2025-08-11 10:22:10 +0800

    FeedbackFacadeImpl - submitFeedback

--- src/main/java/com/academix/academix/document/feedback/facade/api/FeedbackFacade.java
+++ src/main/java/com/academix/academix/document/feedback/facade/api/FeedbackFacade.java
@@ -1,6 +1,7 @@
 package com.academix.academix.document.feedback.facade.api;
 
 import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
+import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
 import com.academix.academix.document.feedback.entity.Feedback;
 import com.academix.academix.document.request.entity.DocumentRequest;
 import org.springframework.data.domain.Page;
@@ -8,11 +9,11 @@ import org.springframework.data.domain.PageRequest;
 
 public interface FeedbackFacade {
     // Common
-    Feedback submitFeedback(Long documentRequestId, FeedbackRequestDTO feedbackRequestDTO);
-    Feedback getFeedbackByRequestId(Long requestId);
+    FeedbackResponseDTO submitFeedback(Long documentRequestId, FeedbackRequestDTO feedbackRequestDTO);
+    FeedbackResponseDTO getFeedbackByRequestId(Long requestId);
 
     // Admin
-    Page<Feedback> getAllFeedbacks(int page, int size, String sortField, String sortDirection);
-    Page<Feedback> getFeedbacksByRating(int rating, int page, int size, String sortField, String sortDirection);
+    Page<FeedbackResponseDTO> getAllFeedbacks(int page, int size, String sortField, String sortDirection);
+    Page<FeedbackResponseDTO> getFeedbacksByRating(int rating, int page, int size, String sortField, String sortDirection);
 
 }
--- src/main/java/com/academix/academix/document/feedback/facade/impl/FeedbackFacadeImpl.java
+++ src/main/java/com/academix/academix/document/feedback/facade/impl/FeedbackFacadeImpl.java
@@ -1,11 +1,13 @@
 package com.academix.academix.document.feedback.facade.impl;
 
 import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
+import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
 import com.academix.academix.document.feedback.entity.Feedback;
 import com.academix.academix.document.feedback.facade.api.FeedbackFacade;
 import com.academix.academix.document.feedback.mapper.FeedbackMapper;
 import com.academix.academix.document.feedback.service.api.FeedbackService;
 import com.academix.academix.document.request.entity.DocumentRequest;
+import com.academix.academix.document.request.service.api.DocumentRequestService;
 import com.academix.academix.log.service.api.DocumentRequestAuditService;
 import com.academix.academix.user.service.api.UserService;
 import lombok.RequiredArgsConstructor;
@@ -21,25 +23,31 @@ public class FeedbackFacadeImpl implements FeedbackFacade {
     private final FeedbackMapper feedbackMapper;
     private final UserService userService; //for logs
     private final DocumentRequestAuditService documentRequestAuditService;
-
+    private final DocumentRequestService documentRequestService;
 
     @Override
-    public Feedback submitFeedback(Long documentRequestId, FeedbackRequestDTO feedbackRequestDTO) {
+    public FeedbackResponseDTO submitFeedback(Long documentRequestId, FeedbackRequestDTO feedbackRequestDTO) {
+        DocumentRequest documentRequest = documentRequestService.fetchDocumentRequestById(documentRequestId);
+        Feedback feedback = feedbackMapper.toFeedback(feedbackRequestDTO);
+        Feedback savedFeedback = feedbackService.submitFeedback(documentRequest, feedback);
+
+        // logs, make it AOP
+
+        return feedbackMapper.toFeedbackResponseDTO(savedFeedback);
+    }
+
+    @Override
+    public FeedbackResponseDTO getFeedbackByRequestId(Long requestId) {
         return null;
     }
 
     @Override
-    public Feedback getFeedbackByRequestId(Long requestId) {
+    public Page<FeedbackResponseDTO> getAllFeedbacks(int page, int size, String sortField, String sortDirection) {
         return null;
     }
 
     @Override
-    public Page<Feedback> getAllFeedbacks(int page, int size, String sortField, String sortDirection) {
-        return null;
-    }
-
-    @Override
-    public Page<Feedback> getFeedbacksByRating(int rating, int page, int size, String sortField, String sortDirection) {
+    public Page<FeedbackResponseDTO> getFeedbacksByRating(int rating, int page, int size, String sortField, String sortDirection) {
         return null;
     }
 }
