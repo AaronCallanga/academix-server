21270b41b3d36dc1888eaf3450be19f57b8ecf6a
pogi <aaroncallanga01@gmail.com>
2025-08-11 08:27:08 +0800

    submitFeedback refactored

--- src/main/java/com/academix/academix/document/feedback/service/api/FeedbackService.java
+++ src/main/java/com/academix/academix/document/feedback/service/api/FeedbackService.java
@@ -3,6 +3,7 @@ package com.academix.academix.document.feedback.service.api;
 import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
 import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
 import com.academix.academix.document.feedback.entity.Feedback;
+import com.academix.academix.document.request.entity.DocumentRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public interface FeedbackService {
     // Common
-    Feedback submitFeedback(FeedbackRequestDTO feedbackRequestDTO, Authentication authentication);
+    Feedback submitFeedback(DocumentRequest documentRequest, Feedback feedbackRequest, Authentication authentication);
     Feedback getFeedbackByRequestId(Long requestId);
     List<FeedbackResponseDTO> getOwnFeedbacks(Authentication authentication);
 
--- src/main/java/com/academix/academix/document/feedback/service/impl/FeedbackServiceImpl.java
+++ src/main/java/com/academix/academix/document/feedback/service/impl/FeedbackServiceImpl.java
@@ -3,16 +3,38 @@ package com.academix.academix.document.feedback.service.impl;
 import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
 import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
 import com.academix.academix.document.feedback.entity.Feedback;
+import com.academix.academix.document.feedback.mapper.FeedbackMapper;
+import com.academix.academix.document.feedback.repository.FeedbackRepository;
 import com.academix.academix.document.feedback.service.api.FeedbackService;
+import com.academix.academix.document.request.entity.DocumentRequest;
+import com.academix.academix.document.request.enums.DocumentStatus;
+import com.academix.academix.exception.types.BadRequestException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class FeedbackServiceImpl implements FeedbackService {
+
+    private final FeedbackRepository feedbackRepository;
+
     @Override
-    public Feedback submitFeedback(FeedbackRequestDTO feedbackRequestDTO, Authentication authentication) {
+    public Feedback submitFeedback(DocumentRequest documentRequest, Feedback feedbackRequest, Authentication authentication) {
+
+        if (!documentRequest.getStatus().equals(DocumentStatus.RELEASED) || !documentRequest.getStatus().equals(DocumentStatus.CANCELLED)) {
+            throw new BadRequestException("Request is not yet completed");
+        }
+
+        // FeedbackRequest mapped by mapper in facade has rating, comment, anonymous
+
+
+
+
         return null;
     }
 
