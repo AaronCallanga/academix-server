c6d21e87d18bef6c4bc51b379eaa3a7d45bbacc7
pogi <aaroncallanga01@gmail.com>
2025-08-12 08:51:48 +0800

    FeedbackController set up

--- src/main/java/com/academix/academix/document/feedback/controller/FeedbackController.java
+++ src/main/java/com/academix/academix/document/feedback/controller/FeedbackController.java
@@ -1,4 +1,18 @@
 package com.academix.academix.document.feedback.controller;
 
+import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
+import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
+import com.academix.academix.document.feedback.facade.api.FeedbackFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/feedbacks")
+@RequiredArgsConstructor
 public class FeedbackController {
+
+
 }
