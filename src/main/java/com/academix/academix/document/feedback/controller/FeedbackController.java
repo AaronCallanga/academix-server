ed377c8730fc23ce213b6cc0295ea5103493e6a2
pogi <aaroncallanga01@gmail.com>
2025-08-12 08:53:20 +0800

    FeedbackController - submitFeedback

--- src/main/java/com/academix/academix/document/feedback/controller/FeedbackController.java
+++ src/main/java/com/academix/academix/document/feedback/controller/FeedbackController.java
@@ -4,6 +4,9 @@ import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
 import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
 import com.academix.academix.document.feedback.facade.api.FeedbackFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/feedbacks")
 @RequiredArgsConstructor
 public class FeedbackController {
+
+    private final FeedbackFacade feedbackFacade;
+
+    @PostMapping("/documents/{documentRequestId}")
+    public ResponseEntity<FeedbackResponseDTO> submitFeedback(@RequestBody FeedbackRequestDTO feedbackRequestDTO, @PathVariable Long documentRequestId) {
+        FeedbackResponseDTO feedbackResponseDTO = feedbackFacade.submitFeedback(documentRequestId, feedbackRequestDTO);
+        return new ResponseEntity<>(feedbackResponseDTO, HttpStatus.CREATED);
+    }
+
 
 
 }
