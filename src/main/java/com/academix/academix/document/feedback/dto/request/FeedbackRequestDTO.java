0b632a598a6e1f1b72e1c66433cd85473ca8b96d
pogi <aaroncallanga01@gmail.com>
2025-08-17 22:35:30 +0800

    FeedbackRequestDTO

--- src/main/java/com/academix/academix/document/feedback/dto/request/FeedbackRequestDTO.java
+++ src/main/java/com/academix/academix/document/feedback/dto/request/FeedbackRequestDTO.java
@@ -1,19 +1,10 @@
 package com.academix.academix.document.feedback.dto.request;
 
-import com.academix.academix.document.request.entity.DocumentRequest;
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
