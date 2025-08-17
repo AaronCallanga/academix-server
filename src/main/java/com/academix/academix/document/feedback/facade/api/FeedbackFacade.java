d2fd9581647ce89750c31bdb6ab8419d5fc29afb
pogi <aaroncallanga01@gmail.com>
2025-08-17 22:37:45 +0800

    FeedbackFacade

--- src/main/java/com/academix/academix/document/feedback/facade/api/FeedbackFacade.java
+++ src/main/java/com/academix/academix/document/feedback/facade/api/FeedbackFacade.java
@@ -2,10 +2,7 @@ package com.academix.academix.document.feedback.facade.api;
 
 import com.academix.academix.document.feedback.dto.request.FeedbackRequestDTO;
 import com.academix.academix.document.feedback.dto.response.FeedbackResponseDTO;
-import com.academix.academix.document.feedback.entity.Feedback;
-import com.academix.academix.document.request.entity.DocumentRequest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
 public interface FeedbackFacade {
     // Common
