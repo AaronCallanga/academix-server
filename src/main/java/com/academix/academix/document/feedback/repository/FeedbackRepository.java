f821c00b30c3f0b75f1c9b6f46d5f8e996c8393f
pogi <aaroncallanga01@gmail.com>
2025-08-22 10:20:27 +0800

    FeedbackRepository

--- src/main/java/com/academix/academix/document/feedback/repository/FeedbackRepository.java
+++ src/main/java/com/academix/academix/document/feedback/repository/FeedbackRepository.java
@@ -1,11 +1,9 @@
 package com.academix.academix.document.feedback.repository;
 
 import com.academix.academix.document.feedback.entity.Feedback;
-import com.academix.academix.document.request.entity.DocumentRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
