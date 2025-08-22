f1e3c43fdd9572df2e6dc7220bc48dce6e7f3701
pogi <aaroncallanga01@gmail.com>
2025-08-22 10:31:19 +0800

    FeedbackRepository - findAverageRating

--- src/main/java/com/academix/academix/document/feedback/repository/FeedbackRepository.java
+++ src/main/java/com/academix/academix/document/feedback/repository/FeedbackRepository.java
@@ -4,8 +4,10 @@ import com.academix.academix.document.feedback.entity.Feedback;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,7 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     // _Id lets Spring Data know you want to match on the foreign key column (document_request_id)
     Optional<Feedback> findByDocumentRequest_Id(Long id);
     Page<Feedback> findByRating(int rating, Pageable pageable);
+
+    @Query("SELECT AVG(f.rating) FROM Feedback f")
+    Double findAverageRating();
 }
