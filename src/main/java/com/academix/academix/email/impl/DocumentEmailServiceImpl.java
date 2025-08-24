4b26b326db99f1fe1648a85cc198f53c3430ba23
pogi <aaroncallanga01@gmail.com>
2025-08-24 18:46:17 +0800

    DocumentEmailServiceImpl - sendDocumentComplete

--- src/main/java/com/academix/academix/email/impl/DocumentEmailServiceImpl.java
+++ src/main/java/com/academix/academix/email/impl/DocumentEmailServiceImpl.java
@@ -7,6 +7,7 @@ import com.academix.academix.user.entity.User;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
@@ -18,6 +19,7 @@ public class DocumentEmailServiceImpl extends BaseEmailServiceImpl implements Do
         super(mailSender);
     }
 
+    @Async("emailExecutor")
     @Override
     public void sendDocumentUpdate(User user, DocumentRequest documentRequest) throws MessagingException, UnsupportedEncodingException {
         String toAddress = user.getEmail();
@@ -44,7 +46,20 @@ public class DocumentEmailServiceImpl extends BaseEmailServiceImpl implements Do
     @Override
     public void sendDocumentComplete(User user,
                                      DocumentRequest documentRequest) throws MessagingException, UnsupportedEncodingException {
+        String toAddress = user.getEmail();
+        String subject = "Your Document Request is Ready";
+        String content = "Dear [[name]],<br>"
+                + "Good news! Your document request (ID: [[requestId]]) has been completed.<br>"
+                + "Please don’t forget to provide us with feedback to help improve our services.<br>"
+                + "<br><a href=\"[[feedbackURL]]\">Leave Feedback</a><br><br>"  // sample url, if you got frontend
+                + "Thank you,<br>"
+                + "<b>Academix Team</b>";
 
+        content = content.replace("[[name]]", user.getName())
+                         .replace("[[requestId]]", documentRequest.getId().toString())
+                         .replace("[[feedbackURL]]", "https://academix.com/feedback/" + documentRequest.getId());
+
+        sendEmail(toAddress, subject, content);
     }
 
     @Override
