4a77ebd7af3f2d749c8e25f422142baf857a5961
pogi <aaroncallanga01@gmail.com>
2025-08-25 08:29:37 +0800

    FeedbackEmailServiceImpl set up

---
+++ src/main/java/com/academix/academix/email/impl/FeedbackEmailServiceImpl.java
@@ -0,0 +1,29 @@
+package com.academix.academix.email.impl;
+
+import com.academix.academix.document.feedback.entity.Feedback;
+import com.academix.academix.email.BaseEmailServiceImpl;
+import com.academix.academix.email.api.FeedbackEmailService;
+import com.academix.academix.user.entity.User;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class FeedbackEmailServiceImpl extends BaseEmailServiceImpl implements FeedbackEmailService {
+
+    public FeedbackEmailServiceImpl(JavaMailSender mailSender) {
+        super(mailSender);
+    }
+
+    @Override
+    public void sendLowRatingSupport(User user, Feedback feedback) {
+
+    }
+
+    @Override
+    public void sendAppreciation(User user, Feedback feedback) {
+
+    }
+
+    @Override
+    public void sendNeutralAcknowledgement(User user, Feedback feedback) {
+
+    }
+}
