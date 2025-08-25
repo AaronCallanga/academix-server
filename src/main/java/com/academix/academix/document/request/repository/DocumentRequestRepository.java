3d3a5e7a8337d18dc73b1b4acd5f2e4c494349c6
pogi <aaroncallanga01@gmail.com>
2025-08-25 09:13:49 +0800

    DocumentRequestRepository - findRequestCompletedWithoutFeedback

--- src/main/java/com/academix/academix/document/request/repository/DocumentRequestRepository.java
+++ src/main/java/com/academix/academix/document/request/repository/DocumentRequestRepository.java
@@ -4,10 +4,25 @@ import com.academix.academix.document.request.entity.DocumentRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface DocumentRequestRepository extends JpaRepository<DocumentRequest, Long> {
     Page<DocumentRequest> findByRequestedById(Long userId, Pageable pageable);
     Page<DocumentRequest> findByRequestedByEmail(String email, Pageable pageable);
+
+    @Query("""
+    SELECT dr 
+    FROM DocumentRequest dr
+    WHERE dr.status = 'RELEASED'
+      AND dr.pickUpDate < :cutoffDate
+      AND NOT EXISTS (
+          SELECT f FROM Feedback f WHERE f.documentRequest = dr
+      )
+    """)
+    List<DocumentRequest> findRequestCompletedWithoutFeedback(LocalDateTime cutoffDate);
 }
--- src/main/java/com/academix/academix/scheduler/FeedbackScheduledTask.java
+++ src/main/java/com/academix/academix/scheduler/FeedbackScheduledTask.java
@@ -1,22 +1,41 @@
 package com.academix.academix.scheduler;
 
+import com.academix.academix.document.feedback.entity.Feedback;
+import com.academix.academix.document.feedback.repository.FeedbackRepository;
 import com.academix.academix.document.feedback.service.api.FeedbackService;
+import com.academix.academix.email.api.FeedbackEmailService;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
+
 @Component
 @Slf4j
 public class FeedbackScheduledTask {
 
-    private final TaskScheduler taskScheduler;
-    private final FeedbackService feedbackService;
+    private final TaskScheduler feedbackScheduler;
+    private final FeedbackEmailService feedbackEmailService;
+    private final FeedbackRepository feedbackRepository;
 
-    public FeedbackScheduledTask(@Qualifier("feedbackScheduler") TaskScheduler taskScheduler, FeedbackService feedbackService) {
-        this.taskScheduler = taskScheduler;
-        this.feedbackService = feedbackService;
+    public FeedbackScheduledTask(@Qualifier("feedbackScheduler") TaskScheduler feedbackScheduler,
+                                 FeedbackEmailService feedbackEmailService, FeedbackRepository feedbackRepository) {
+        this.feedbackScheduler = feedbackScheduler;
+        this.feedbackEmailService = feedbackEmailService;
+        this.feedbackRepository = feedbackRepository;
     }
 
+    @PostConstruct
+    public void init() {
+        feedbackScheduler.scheduleAtFixedRate(this::sendFeedbackEmailReminder, );
+    }
 
+    private void sendFeedbackEmailReminder() {
+        List<Feedback> feedbacks = feedbackRepository.findAll();
+        feedbackEmailService.notifyFeedbackReminder(user, feedback);
+    }
 }
