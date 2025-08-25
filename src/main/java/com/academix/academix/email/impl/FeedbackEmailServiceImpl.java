676981c954a1798610da52df54e69c2dca2fe2ae
pogi <aaroncallanga01@gmail.com>
2025-08-25 08:38:09 +0800

    FeedbackEmailServiceImpl - sendAppreciation

--- src/main/java/com/academix/academix/email/impl/FeedbackEmailServiceImpl.java
+++ src/main/java/com/academix/academix/email/impl/FeedbackEmailServiceImpl.java
@@ -22,21 +22,36 @@ public class FeedbackEmailServiceImpl extends BaseEmailServiceImpl implements Fe
                 + "We noticed that you rated your recent document request (ID: [[requestId]]) with a score of <b>[[rating]]</b>.<br>"
                 + "We’re sorry that your experience didn’t meet expectations.<br><br>"
                 + "Our support team would love to hear from you and help resolve any issues you faced.<br>"
-                + "Please reach out to us at <a href=\"mailto:aaroncallanga@gmail.com\">aaroncallanga@gmail.com</a>.<br><br>" // support@academix.com
+                + "Please reach out to us at <a href=\"[[mailTo]]\">[[supportEmail]]</a>.<br><br>" // support@academix.com
                 + "Thank you for helping us improve our services.<br><br>"
                 + "Best regards,<br>"
                 + "<b>Academix Support Team</b>";
 
         content = content.replace("[[name]]", user.getName())
                          .replace("[[requestId]]", feedback.getDocumentRequest().getId().toString())
-                         .replace("[[rating]]", String.valueOf(feedback.getRating()));
+                         .replace("[[rating]]", String.valueOf(feedback.getRating()))
+                         .replace("[[mailTo]]", "mailto:aaroncallanga@gmail.com")
+                         .replace("[[supportEmail]]", "aaroncallanga@gmail.com");
 
         sendEmail(toAddress, subject, content);
     }
 
     @Override
     public void sendAppreciation(User user, Feedback feedback) {
+        String toAddress = user.getEmail();
+        String subject = "Thank You for Your Feedback!";
+        String content = "Dear [[name]],<br><br>"
+                + "Thank you for rating your recent document request (ID: [[requestId]]) with a score of <b>[[rating]]</b>.<br>"
+                + "We truly appreciate your positive feedback and are glad we could serve you well.<br><br>"
+                + "Your support motivates us to continue improving Academix.<br><br>"
+                + "Warm regards,<br>"
+                + "<b>Academix Team</b>";
 
+        content = content.replace("[[name]]", user.getName())
+                         .replace("[[requestId]]", feedback.getDocumentRequest().getId().toString())
+                         .replace("[[rating]]", String.valueOf(feedback.getRating()));
+
+        sendEmail(toAddress, subject, content);
     }
 
     @Override
