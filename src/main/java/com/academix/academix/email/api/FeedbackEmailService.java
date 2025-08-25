ad222ac266f0534948afbe1bb9e502504422a8c0
pogi <aaroncallanga01@gmail.com>
2025-08-25 08:28:47 +0800

    FeedbackEmailService set up

--- src/main/java/com/academix/academix/email/api/FeedbackEmailService.java
+++ src/main/java/com/academix/academix/email/api/FeedbackEmailService.java
@@ -1,7 +1,11 @@
 package com.academix.academix.email.api;
 
+import com.academix.academix.document.feedback.entity.Feedback;
 import com.academix.academix.email.EmailService;
+import com.academix.academix.user.entity.User;
 
 public interface FeedbackEmailService extends EmailService {
-
+    void sendLowRatingSupport(User user, Feedback feedback);
+    void sendAppreciation(User user, Feedback feedback);
+    void sendNeutralAcknowledgement(User user, Feedback feedback);
 }
