23e6f07569f0dee2c6f9013830cb945ee67fbefe
pogi <aaroncallanga01@gmail.com>
2025-08-26 09:56:36 +0800

    SchedulerConfig - documentScheduler

--- src/main/java/com/academix/academix/config/SchedulerConfig.java
+++ src/main/java/com/academix/academix/config/SchedulerConfig.java
@@ -41,6 +41,15 @@ public class SchedulerConfig implements SchedulingConfigurer {
         return scheduler;
     }
 
+    @Bean(name = "documentScheduler")
+    public TaskScheduler documentScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("documentScheduler-");
+        scheduler.initialize();
+        return scheduler;
+    }
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
             taskRegistrar.setTaskScheduler(defaultScheduler());
--- src/main/java/com/academix/academix/scheduler/DocumentScheduledTask.java
+++ src/main/java/com/academix/academix/scheduler/DocumentScheduledTask.java
@@ -2,11 +2,16 @@ package com.academix.academix.scheduler;
 
 import com.academix.academix.email.api.DocumentEmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+@Slf4j
 public class DocumentScheduledTask {
 
     private final DocumentEmailService documentEmailService;
+    private final TaskExecutor emailTaskExecutor;
+
+
 }
