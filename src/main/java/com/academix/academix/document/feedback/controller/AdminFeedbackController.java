052aa54291764172d41925f1f494f40325266389
pogi <aaroncallanga01@gmail.com>
2025-08-28 10:13:41 +0800

    RBAC - AdminFeedbackController

--- src/main/java/com/academix/academix/document/feedback/controller/AdminFeedbackController.java
+++ src/main/java/com/academix/academix/document/feedback/controller/AdminFeedbackController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/admin/feedbacks")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminFeedbackController {
 
     private final FeedbackFacade feedbackFacade;
