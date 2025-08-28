163f1a4093be342885f82fae9a6689341fb443a6
pogi <aaroncallanga01@gmail.com>
2025-08-28 10:28:04 +0800

    DocumentRequestController - RBAC

--- src/main/java/com/academix/academix/document/request/controller/DocumentRequestController.java
+++ src/main/java/com/academix/academix/document/request/controller/DocumentRequestController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/documents")
 @RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ADMIN', 'REGISTRAR', 'STUDENT')")
 public class DocumentRequestController {
 
     private final DocumentRequestFacade documentRequestFacade;
