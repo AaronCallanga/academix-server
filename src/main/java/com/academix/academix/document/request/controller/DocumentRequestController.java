94aecbfd7c8d15a5925c76186bf63f0554ef9e48
pogi <aaroncallanga01@gmail.com>
2025-08-29 10:58:17 +0800

    DocumentRequestController - getAllOwnDocumentRequests

--- src/main/java/com/academix/academix/document/request/controller/DocumentRequestController.java
+++ src/main/java/com/academix/academix/document/request/controller/DocumentRequestController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,11 +27,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/documents")
 @RequiredArgsConstructor
-@PreAuthorize("hasAnyRole('ADMIN', 'REGISTRAR', 'STUDENT')")
 public class DocumentRequestController {
 
     private final DocumentRequestFacade documentRequestFacade;
 
+    @PreAuthorize("hasRole('STUDENT')")
     @GetMapping("/own")
     public ResponseEntity<Page<DocumentRequestResponseListDTO>> getAllOwnDocumentRequests(
             Authentication authentication,
