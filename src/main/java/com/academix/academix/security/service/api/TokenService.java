81b50564746f4098dde31e7cc2b168a212f5d21a
pogi <aaroncallanga01@gmail.com>
2025-09-02 19:13:47 +0800

    TokenService clean up

--- src/main/java/com/academix/academix/security/service/api/TokenService.java
+++ src/main/java/com/academix/academix/security/service/api/TokenService.java
@@ -3,8 +3,6 @@ package com.academix.academix.security.service.api;
 import com.academix.academix.security.entity.VerificationToken;
 import com.academix.academix.user.entity.User;
 
-import java.util.Optional;
-
 public interface TokenService {
     VerificationToken generateToken(User user);
     void deleteToken(VerificationToken token);
